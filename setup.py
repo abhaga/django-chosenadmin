5e1358954fbeb930fdc01b2e2186705b6b5b5387
Abhaya Agarwal <abhaya.agarwal@gmail.com>
2016-07-26 17:06:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     packages=[
         "chosenadmin",
     ],
+    include_package_data=True,
     package_data={'chosenadmin': ['templates/**/*', 'static/**/*']},
     long_description="Adds the Chosen.js plugin to Select and Multi-select elements in Django's admin.",
     classifiers=[
